1241b1f1f2110e99f8669deae1f15b11c4011b6e
Scott Hayward <scotthayward@gmail.com>
2016-10-25 14:17:14 -0400

    completed multiple returns

---
+++ multipleReturns.py
@@ -0,0 +1,26 @@
+# LESSON
+
+def getPerson():
+        name = "Leona"
+        age = 35
+        country = "UK"
+        return name, age, country
+
+name, age, country = getPerson()
+print(name)
+print(age)
+print(country)
+
+# EXERCISES
+
+def someNumbers():
+    a = 69
+    b = 96
+    return a, b
+
+a, b = someNumbers()
+c = a + b
+print(a)
+print(b)
+print(c)
+
